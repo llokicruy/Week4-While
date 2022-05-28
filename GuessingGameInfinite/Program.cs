75a6444b994573ebca7eb71831445f09ec78557f
Jüri Kollo <jkollo@tktk.ee>
2022-05-28 16:11:58 +0300

    add new projects

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ara arvama;
+            //kui kasutaja suutis seda numbrit ara arvata, siis ta on mangu voitnud;
+            //katsete arv on piiramatu;
+            //*programm genereerib juhuslikku numbrit uhte korda;
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            bool loopActive = true; //boolean --> true/false
+
+            while (loopActive)
+            {
+                Console.WriteLine("Sisesta number 1 kuni 10:");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber == cpuNumber)
+                {
+                    Console.WriteLine("Oled võitnud!");
+                    loopActive = false;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ara arvama;
+            //kui kasutaja suutis seda numbrit ara arvata, siis ta on mangu voitnud;
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme katsega numbrit ara arvata;
+            //siis mangu võidab arvuti;
+            //*programm genereerib juhuslikku numbrit uhte korda;
+
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            int i = 0;
+
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta number 1 kuni 10:");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber == cpuNumber)
+                {
+                    Console.WriteLine("Oled võitnud!");
+                    break;
+                }
+                else
+                {
+                    //(i = i + 1) == i++ 
+                    i++;
+                    Console.WriteLine($"Vale number. {3 - i} katset on jäänud.");
+
+                }
+            }
+        }
+    }
+}
